cc3774fcbb5355e79ab92ae0239d8cc3caff0631
kktlove <kkt09072@gmail.com>
2023-08-18 11:47:00 +0900

    add to Custom

---
+++ src/edu/chunjae/model/CustomDAO.java
@@ -0,0 +1,28 @@
+package edu.chunjae.model;
+
+import edu.chunjae.dto.Custom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomDAO {
+    public List<Custom> getCustomList(){
+        List<Custom> cusList = new ArrayList<>();
+
+        return cusList;
+    }
+
+    public Custom getCustom(String id){
+        Custom cus = new Custom();
+
+        return cus;
+    }
+
+    public boolean login(String id, String pw){
+        boolean pass = false;
+
+        return pass;
+    }
+
+
+}
